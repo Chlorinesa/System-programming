361aee8b23f40cda1fc6b58173f24b326fb6853d
Chlorinesa <154625279+Chlorinesa@users.noreply.github.com>
2025-09-13 00:06:48 +0300

    Add files via upload
    In a sequence of non-negative integers not exceeding 10000, with a size of 1000 elements, find a number R that satisfies the following conditions:
    R is minimal; R is divisible by 21; R is the product of two different elements of the sequence;
    if there is no such number, output -1

---
+++ ConsistentSearch.java
@@ -0,0 +1,29 @@
+//Задача:
+//В последовательности неотрицательных целых чисел не превышающих 10000, размером 1000 элементов найти число R удовлетворяющие следующим условиям:
+//R минимально;
+//R кратно 21;
+//R произведение 2х различных элементов последовательности;
+//если такого числа нет вывести -1
+public class ConsistentSearch {
+    public static void main(String[] args) {
+        int[] mas = new int[1000];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = ((int) (Math.random() * 10001));
+        }
+        int minMul = -1;
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = i + 1; j < mas.length; j++) {
+                int first = mas[i];
+                int second = mas[j];
+                int mul = first * second;
+                if (mul % 21 == 0) {
+                    if (minMul==-1||mul < minMul) {
+                        minMul = mul;
+//                      System.out.println(first + " " + second);
+                    }
+                }
+            }
+        }
+        System.out.println(minMul);
+    }
+}
