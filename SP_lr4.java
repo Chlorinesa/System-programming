139fe36edcaf2fdf11a78c6e1fda3a50eeb39561
Chlorinesa <154625279+Chlorinesa@users.noreply.github.com>
2025-09-27 14:37:23 +0300

    Add files via upload
    Methods isAlive() and join(). Develop an application that allows you to resolve the dispute: "Which came first - the chicken or the egg?"
    One thread should display the message "Chicken", while the other thread should display the message "Egg".
    As a result of the application's operation, the dispute should be resolved, and depending on which thread had the last word

---
+++ SP_lr4.java
@@ -0,0 +1,64 @@
+class Chicken implements Runnable {
+    String name;
+    Chicken(String name) {
+        this.name = name;
+    }
+    @Override
+    public void run() {
+        for(int i = 0; i< 8; i++){
+            try {
+                Thread.sleep(800);
+                Main.eggOrChicken(name);
+                System.out.println("\u001B[31m"+name + "!");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+    }
+}
+class Egg implements Runnable {
+    String name;
+    Egg(String name) {
+        this.name = name;
+    }
+    @Override
+    public void run() {
+        for(int i = 0; i<8; i++){
+            try {
+                Thread.sleep(800);
+                Main.eggOrChicken(name);
+                System.out.println("\u001B[33m "+name+ "!");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+    }
+}
+
+public class SP_lr4{
+    private static String lastWord = "";
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) {
+        Thread chicken = new Thread(new Chicken("Курица"));
+        Thread egg = new Thread(new Egg("Яйцо"));
+        chicken.start();
+        egg.start();
+        if (egg.isAlive()&&chicken.isAlive()) {
+            try {
+                chicken.join();
+                egg.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("\u001B[32m " + "в споре выяснилось, что первым появилось: " + lastWord+ "!");
+        }
+    }
+    public static void eggOrChicken(String name) {
+        synchronized (lock) {
+            lastWord = name;
+        }
+    }
+}
